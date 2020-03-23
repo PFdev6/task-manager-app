573c389e00174622ff9db1e85a568f44f8e25315
PFdev <fedorovich2012bel@gmail.com>
2020-03-24 00:22:11 +0300

    #3 - Authentication: Fix creating users in db after sign up

--- controllers/api/authentication_controller.js
+++ controllers/api/authentication_controller.js
@@ -1,4 +1,4 @@
-const User = require("../../models/user");
+const db = require("../../models");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
@@ -6,8 +6,7 @@ module.exports.signUp = (req, res) => {
   console.log(req.body);
   const { email, name, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, saltRounds);
-  const user = User.create({ email, name, hashedPassword })
+  db.User.create({ email, name, hashedPassword })
     .then(user => res.status(201).send(user))
     .catch(error => res.status(400).send(error));
-  console.log(user);
 };
