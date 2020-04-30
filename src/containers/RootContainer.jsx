8943388eb1db8f5274cc173ca508b0eb464550ff
PFdev <fedorovich2012bel@gmail.com>
2020-04-30 19:06:29 +0300

    #8 - Websockets: Show Notifications

--- src/containers/RootContainer.jsx
+++ src/containers/RootContainer.jsx
@@ -5,6 +5,9 @@ import Entry from "./Entry";
 import Main from "./Main";
 import { DEFAULT_USER_AUTH } from "../utils/Consts";
 import useSocket from "../utils/custom-hooks/SocketHandler";
+import ReactNotification from "react-notifications-component";
+import "react-notifications-component/dist/theme.css";
+import { store } from "react-notifications-component";
 
 const RootContainer = () => {
   const url = "localhost:8000";
@@ -14,8 +17,27 @@ const RootContainer = () => {
       ? useSocket(url, auth.id)
       : { note: {}, isConnected: false };
 
+  React.useEffect(() => {
+    store.addNotification({
+      title: "Task Update",
+      message: note.message || "test",
+      type: "info",
+      insert: "top",
+      container: "top-right",
+      animationIn: ["animated", "fadeIn"],
+      animationOut: ["animated", "fadeOut"],
+      dismiss: {
+        duration: 5000,
+        onScreen: true
+      }
+    });
+  });
+
   return (
-    <Wrapper>{auth.id === DEFAULT_USER_AUTH.id ? <Entry /> : <Main />}</Wrapper>
+    <Wrapper>
+      <ReactNotification />
+      {auth.id === DEFAULT_USER_AUTH.id ? <Entry /> : <Main />}
+    </Wrapper>
   );
 };
 
